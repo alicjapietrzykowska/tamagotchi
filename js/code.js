f4ab1d672e3b6abf4271e2b0785808ca64bdc15e
Alicja <alicja.x.lukaszewska@gmail.com>
2017-11-30 00:12:12 +0100

    backup before objects

--- js/code.js
+++ js/code.js
@@ -4,43 +4,74 @@ const reviveBtn = document.querySelector('#revive');
 
 const animal = document.querySelector('.animal img');
 
-function takeCare () {
-	stats.forEach(stat => {
-		if (stat.dataset.need === this.dataset.need){
-			stat.value++;
-		}
-		if (this.dataset.need === 'eat'){
-			animal.src = "img/eat.png";
-		}
-		if (this.dataset.need === 'cure'){
-			animal.src = "img/shower.png";
-		}
-		if (this.dataset.need === 'pet'){
-			animal.src = "img/laughing.png";
-		}
-	});
+let pressTimer;
+
+function takeCare (e) {
+	pressTimer = setInterval(function() {
+		stats.forEach(stat => {
+			target = e.target;
+			if (stat.dataset.need === target.dataset.need){
+				stat.value++;
+			}
+			if (target.dataset.need === 'eat'){
+				animal.src = "img/eat.png";
+			}
+			if (target.dataset.need === 'cure'){
+				animal.src = "img/shower.png";
+			}
+			if (target.dataset.need === 'pet'){
+				animal.src = "img/laughing.png";
+			}
+		});
+	}, 150)
 }
 
-function leaveCare () {
-	animal.src = "img/hello.png";
+const waitingIcons = ['cool', 'music', 'selfie'];
+
+const needs = [];
+
+function checkMood() {
+	stats.forEach(stat => {
+		if (stat.value < 30) {
+			animal.removeEventListener('mousedown', clicked);
+			if (needs.includes(stat.dataset.need)) return;
+			needs.push(stat.dataset.need);
+			console.log(needs);
+				
+
+
+			// if (needs.includes("cure")) 
+			// 	{animal.src = "img/sick.png"}; 
+			// if (needs.includes("eat")) 
+			// 	{animal.src = "img/sad.png"};
+			// if (needs.includes("pet")) 
+			// 	{animal.src = "img/cry.png"};
+		// } else if (stat.value > 31) {
+		// 	animal.src = "img/hello.png";
+		}
+	})
 }
 
 function decreaseStats () {
 	reviveBtn.style.display = "none";
 	let called = setInterval (() => {
 		stats.forEach(stat => {
-			stat.value--;	
+			stat.value--;
+			checkMood();	
 			if (stat.value == 0) {
 				death();
 				clearInterval(called);
 				return;
 			}
 		});
-	}, 60000);
+	}, 300);
 }
 
 
 function death () {
+	buttons.forEach(button => {
+		button.disable = true;
+	})
 	console.log('DEATH!');
 	reviveBtn.style.display = "block";
 	reviveBtn.addEventListener('click', revive);
@@ -51,7 +82,12 @@ function revive () {
 	decreaseStats();
 }
 
+function clicked (e) {
+	animal.src = "img/happy.png"
+};
+
 
 buttons.forEach(button => button.addEventListener('mousedown', takeCare));
-buttons.forEach(button => button.addEventListener('mouseup', leaveCare));
+document.addEventListener('mouseup', () => clearTimeout(pressTimer));
 window.addEventListener('load', decreaseStats);
+animal.addEventListener('mousedown', clicked);
