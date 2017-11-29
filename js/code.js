ee94f33d0b2dbe2bf73f5964745f0ad577859b69
Alicja <alicja.x.lukaszewska@gmail.com>
2017-11-29 15:41:26 +0100

    Added image which change on click

--- js/code.js
+++ js/code.js
@@ -2,12 +2,27 @@ const stats = document.querySelectorAll('.stats input');
 const buttons = document.querySelectorAll('.buttons button');
 const reviveBtn = document.querySelector('#revive');
 
+const animal = document.querySelector('.animal img');
+
 function takeCare () {
 	stats.forEach(stat => {
 		if (stat.dataset.need === this.dataset.need){
 			stat.value++;
 		}
+		if (this.dataset.need === 'eat'){
+			animal.src = "img/eat.png";
+		}
+		if (this.dataset.need === 'cure'){
+			animal.src = "img/shower.png";
+		}
+		if (this.dataset.need === 'pet'){
+			animal.src = "img/laughing.png";
+		}
 	});
+}
+
+function leaveCare () {
+	animal.src = "img/hello.png";
 }
 
 function decreaseStats () {
@@ -15,10 +30,6 @@ function decreaseStats () {
 	let called = setInterval (() => {
 		stats.forEach(stat => {
 			stat.value--;	
-			console.log(stat.value);
-			if (stat.value < 10) {
-
-			}
 			if (stat.value == 0) {
 				death();
 				clearInterval(called);
@@ -41,5 +52,6 @@ function revive () {
 }
 
 
-buttons.forEach(button => button.addEventListener('click', takeCare));
+buttons.forEach(button => button.addEventListener('mousedown', takeCare));
+buttons.forEach(button => button.addEventListener('mouseup', leaveCare));
 window.addEventListener('load', decreaseStats);
