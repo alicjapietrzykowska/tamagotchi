46947123250abc1d8abeaa376d7303c24c7e5e5e
Alicja <alicja.x.lukaszewska@gmail.com>
2017-12-05 15:56:13 +0100

    Functions based on objects

--- js/code.js
+++ js/code.js
@@ -6,105 +6,123 @@ const animal = document.querySelector('.animal img');
 
 let pressTimer;
 let changeIcon;
+let called;
+let flag = false;
 
 const statistics = {
-	hunger: parseFloat(document.querySelector('#hunger').value),
-	health: parseFloat(document.querySelector('#health').value),
-	happiness: parseFloat(document.querySelector('#happiness').value)
+	eat: parseFloat(document.querySelector('#eat').value),
+	cure: parseFloat(document.querySelector('#cure').value),
+	pet: parseFloat(document.querySelector('#pet').value)
 };
 
 function takeCare (e) {
+	clearInterval(changeIcon);
+	flag = true;
 	pressTimer = setInterval(function() {
-		stats.forEach(stat => {
+			Object.entries(statistics).forEach(stat => {
 			target = e.target;
-			if (stat.dataset.need === target.dataset.need){
-				stat.value++;
-			}
-			if (target.dataset.need === 'eat'){
-				animal.src = "img/eat.png";
-			}
-			if (target.dataset.need === 'cure'){
-				console.log('what');
-				animal.src = "img/shower.png";
-			}
-			if (target.dataset.need === 'pet'){
-				animal.src = "img/laughing.png";
+			const name = stat[0];
+			let value = stat[1];
+			if (name === target.dataset.need){
+				value++;
+				statistics[name] = value;
+				document.getElementById(`${name}`).value = value;
+
+				switch(name){
+					case 'eat':
+						animal.src = "img/eat.png";
+						break;
+					case 'cure':
+						animal.src = "img/shower.png";
+						break;
+					case 'pet':
+						animal.src = "img/laughing.png";
+						break;
+					default:
+						defaultState();
+				}
 			}
 		});
-	}, 150)
-	checkMood();
+	}, 100);
 }
+
 
 const waitingIcons = ['hello', 'cool', 'music', 'selfie'];
 
-const needs = [];
-
 let ded = false;
+let sad = false;
 
 function defaultState () {
-		stats.forEach(stat => {
-			if (needs.includes(stat)) needs.splice(needs.indexOf(stat), 1);
-		});	
-		if (ded) return;
-		let miliseconds = Math.floor((Math.random() * (6 - 5) + 5) * 1000);
-		changeIcon = setInterval(function(){
-			let i = Math.floor(Math.random() * (waitingIcons.length - 0));
-			let icon = waitingIcons[i];
-			animal.src = `img/${icon}.png`;
-		}, miliseconds);
-		animal.src = "img/hello.png";
+		if (ded || sad) return;
+		if (!flag){
+			let miliseconds = Math.floor((Math.random() * (6 - 5) + 5) * 1000);
+			changeIcon = setInterval(function(){
+				let i = Math.floor(Math.random() * (waitingIcons.length - 0));
+				let icon = waitingIcons[i];
+				animal.src = `img/${icon}.png`;
+			}, miliseconds);
+			animal.src = "img/hello.png";
+		}
 }
+
 
 function checkMood() {
-	stats.forEach(stat => {
-		if (stat.value < 30) {
-			animal.removeEventListener('mousedown', clicked);
-			if (needs.includes(stat)) return;
-			needs.push(stat);
+	clearInterval(changeIcon);
+	let arr = Object.values(statistics);
+	let min = Math.min(...arr);
+	if (flag) return;
 
-			needs.forEach(need => {
-				console.log(need.dataset.need, need.value);
-				if (need.dataset.need === 'eat'){
-					animal.src = "img/sad.png";
-				}
-				if (need.dataset.need === 'cure'){
-					animal.src = "img/sick.png";
-				}
-				if (need.dataset.need === 'pet'){
-					animal.src = "img/cry.png";
-				}
-			})
+	Object.entries(statistics).forEach(stat => {
+		const name = stat[0];
+		let value = stat[1];
+
+		console.log(value);
+
+		if (value == 0) {
+			death();
+			return;
 		}
 
-		// if (stat.value > 30) {
-		// 	decreaseStats();
-		// }
-	})
+		if (value < 30){
+			sad = true;
+			if (value == min && name === 'eat'){
+				animal.src = "img/sad.png";
+			}
+			if (value == min && name === 'cure'){
+				animal.src = "img/sick.png";
+			}
+			if (value == min && name === 'pet'){
+				animal.src = "img/cry.png";
+			}
+		} else {
+			sad = false;
+		}
+
+	});
 }
+
 
 function decreaseStats () {
 	reviveBtn.style.display = "none";
-	// defaultState();
-	let called = setInterval (() => {
-		stats.forEach(stat => {
-			stat.value--;
-			checkMood();	
-			console.log(stat.value);
-			if (stat.value == 0) {
-				console.log('death...');
-				death();
-				clearInterval(called);
-				return;
-			}
+	called = setInterval (() => {
+		Object.entries(statistics).forEach(stat => {
+			const name = stat[0];
+			let value = stat[1];
+			value--;
+			statistics[name] = value;
+			document.getElementById(`${name}`).value = value;
+			checkMood();
 		});
-	}, 1000);
+	}, 100);
 }
 
 
 function death() {
 	document.removeEventListener('mouseup', stop);
 	animal.removeEventListener('mousedown', clicked);
+	clearInterval(called);
 	clearInterval(changeIcon);
+	clearInterval(pressTimer);
 	buttons.forEach(button => {
 		button.disable = true;
 		button.removeEventListener('mousedown', takeCare)
@@ -122,17 +140,19 @@ function revive () {
 	decreaseStats();
 }
 
-function clicked (e) {
+function clicked () {
 	animal.src = "img/happy.png";
 };
 
 function stop(){
+	flag = false;
 	clearTimeout(pressTimer);
-	decreaseStats();
+	clearInterval(changeIcon);
+	defaultState();
 };
 
 buttons.forEach(button => button.addEventListener('mousedown', takeCare));
 document.addEventListener('mouseup', stop);
 window.addEventListener('load', decreaseStats);
-window.addEventListener('load', defaultState);
+// window.addEventListener('load', defaultState);
 animal.addEventListener('mousedown', clicked);
